07ce3103fb3973774f592fbc1693c937209e6c5f
tHomas H <tom@mathpunk.net>
2018-11-05 09:28:50 -0600

    Horrible dependency problems and fresh start

--- bak/project.clj
+++ bak/project.clj
@@ -1,19 +1,18 @@
-(defproject cyclotron "0.1.0-SNAPSHOT"
+(defproject cyclotron "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [tolitius/xml-in "0.1.0"]
-                 [clj-gitlab "0.1.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [clj-http "3.9.1"]
-                 [cheshire "5.8.1"]
-                 [code-maat "1.0.1"]
                  [org.clojure/data.xml "0.0.8"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.4"]]
-  :ring {:handler cyclotron.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                 ;; [tolitius/xml-in "0.1.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [clojure.java-time "0.3.2"]
+                 ;; [datawalk "0.1.12"]
+                 ;; [clj-jgit "0.8.10"]
+                 ;; [me.raynes/conch "0.8.0"]
+                 ;; dev
+                 [com.cognitect/transcriptor "0.1.5"]]
+  :main ^:skip-aot cyclotron.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- project.clj
+++ project.clj
@@ -1,18 +1,9 @@
-(defproject cyclotron "0.3.0-SNAPSHOT"
+(defproject cyclotron "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [tolitius/xml-in "0.1.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [clojure.java-time "0.3.2"]
-                 [datawalk "0.1.12"]
-                 [clj-jgit "0.8.10"]
-                 [me.raynes/conch "0.8.0"]
-                 ;; dev
-                 [com.cognitect/transcriptor "0.1.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot cyclotron.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
